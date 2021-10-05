daf3903424ae87518194b6758b7209e91dbf0304
Mehul <mgupta320@gatech.edu>
2021-10-04 20:57:57 -0400

    Made data_processing methods into a class for optimization

--- data_processing.py
+++ data_processing.py
@@ -3,47 +3,56 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def csv_to_arrays(file_name):
-    # Load data from the csv file into a workable array
-    total_data = pd.read_csv(file_name, sep=',', header=None)
-    total_data = total_data.to_numpy()
+class ModelData:
+    def __init__(self, file_name, time_steps=10, num_samples=1346):
+        self.data = pd.read_csv(file_name, sep=',', header=None).to_numpy()
+        point_x, point_y = self.data_to_arrays(self.data)
+        self.point_x = point_x
+        self.point_y = point_y
+        time_x, time_y = self.point_to_time(self.point_x, self.point_y, time_steps, num_samples)
+        self.time_x = time_x
+        self.time_y = time_y
+        self.time_steps = time_steps
 
-    # separate input data and labelled outputs for model
-    labels_start = np.where(total_data[0] == 1)[0][0]
-    point_data = total_data[:, :labels_start]
-    point_labels = total_data[:, labels_start:]
+    def data_to_arrays(self, total_data):
+        # separate input data and labelled outputs for model
+        labels_start = np.where(total_data[0] == 1)[0][0]
+        point_data = total_data[:, :labels_start]
+        point_labels = total_data[:, labels_start:]
 
-    # creating time series data with 5 time ordered points and their associated labels
-    time_data_shape = (point_data.shape[0] - 25 * 5, 5, point_data.shape[1])
-    time_data = np.empty(time_data_shape)
-    time_labels = np.empty((time_data.shape[0], point_labels.shape[1]))
-    n = 0
-    for i in range(len(point_data)):
-        if (i + 5) >= len(point_data) or not np.array_equal(point_labels[i], point_labels[i + 5]):
-            n -= 1
-            continue
-        time_labels[n] = point_labels[i]
-        time_data[n] = point_data[i:i + 5]
-        n += 1
+        # returning a tuple of two arrays that have the point data and time data respectively
+        return point_data, point_labels
 
-    # if we want to save these arrays to use later
-    point_x_train, point_x_test, point_y_train, point_y_test = train_test_split(point_data, point_labels, test_size=.33,
-                                                                                random_state=0)
-    time_x_train, time_x_test, time_y_train, time_y_test = train_test_split(time_data, time_labels, test_size=.33,
-                                                                            random_state=0)
-    np.savez("data/np_point_arrays", point_x_train, point_x_test, point_y_train, point_y_test)
-    np.savez("data/np_time_arrays", time_x_train, time_x_test, time_y_train, time_y_test)
+    def point_to_time(self, point_data, point_labels, time_steps=10, num_samples=1346):
+        time_data = np.empty(
+            ((len(point_data) - time_steps * len(point_data) // num_samples), time_steps, point_data.shape[1]))
+        time_labels = np.empty((time_data.shape[0], point_labels.shape[1]))
+        i = 0
+        n = 0
+        while i < len(point_data):
+            if (i + time_steps) >= len(point_data) or i % num_samples > num_samples - time_steps:
+                i += time_steps
+                continue
+            time_data[n] = point_data[i:i + time_steps]
+            time_labels[n] = point_labels[i]
+            n += 1
+            i += 1
+        return point_data, point_labels
 
-    # returning a tuple of two arrays that have the point data and time data respectively
-    return ([point_x_train, point_x_test, point_y_train, point_y_test],
-            [time_x_train, time_x_test, time_y_train, time_y_test])
+    def create_test_train(self, point_or_time, test_size=.33, rand_seed=0, save_name=None):
+        if point_or_time == 'point':
+            data = self.point_x
+            labels = self.point_y
+        elif point_or_time == 'time':
+            data = self.time_x
+            labels = self.time_y
+        x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=test_size, random_state=rand_seed)
+        if save_name:
+            np.savez(f"data/{save_name}", x_train, x_test, y_train, y_test)
+        return x_train, x_test, y_train, y_test
 
-
-def load_arrays(file_name):
-    loaded_arrays = np.load(file_name)
-    data_arrays = (loaded_arrays['arr_0'], loaded_arrays['arr_1'], loaded_arrays['arr_2'], loaded_arrays['arr_3'])
-    return data_arrays
-
-
-point_arrays, time_arrays = csv_to_arrays("data/data.csv")
-point_x_train, point_x_test, point_y_train, point_y_test = load_arrays("data/np_point_arrays.npz")
+'''
+model_data_1 = ModelData("data/data.csv")
+model_data_1.create_test_train('point', save_name="np_point_arrays")
+model_data_1.create_test_train('time', save_name="np_time_arrays")
+'''
